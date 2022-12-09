cfe266d3f6dc1b41c4bab2b43e0c6b298cb10b10
d_ginty4 <declan12ginty@gmail.com>
2022-12-09 00:22:02 +0000

    Organised folders and added react router

---
+++ src/views/App.tsx
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./Home";
+import Layout from "./Nav";
+import Projects from "./Projects";
+import Error from "./Error";
+import Calendar from "./Calendar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="projects" element={<Projects/>}/>
+          <Route path="calendar" element={<Calendar/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
