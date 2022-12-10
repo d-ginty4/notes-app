7b55e32e8833b95e4374f72c3df82b0c18566b48
d_ginty4 <declan12ginty@gmail.com>
2022-12-10 21:44:03 +0000

    Added navbar

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,23 +1,27 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import '../sass/style.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "../sass/style.scss";
+
 import Home from "./Home";
-import Layout from "./Nav";
+import Navbar from "./Nav";
 import Projects from "./Projects";
 import Error from "./Error";
 import Calendar from "./Calendar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="projects" element={<Projects/>}/>
-          <Route path="calendar" element={<Calendar/>}/>
-          <Route path="*" element={<Error/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="calendar" element={<Calendar />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/views/Nav.tsx
+++ src/views/Nav.tsx
@@ -1,28 +1,46 @@
 import { Outlet, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faProjectDiagram,
+  faCalendarAlt,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/calendar">Calendar</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/random">Error</Link>
-          </li>
-        </ul>
-      </nav>
+      <nav className="navbar is-fixed-top is-size-4 has-text-weight-bold has-background-primary ">
+        <div className="navbar-start">
+          <div className="navbar-item has-dropdown is-hoverable">
+            <Link to="/" className="navbar-link">
+              <FontAwesomeIcon className="mr-3" icon={faHouse} />
+              Home
+            </Link>
 
+            <div className="navbar-dropdown has-background-info-light">
+              <Link to="/calendar" className="navbar-item is-size-5">
+                <FontAwesomeIcon className="mr-3" icon={faCalendarAlt} />
+                Calender
+              </Link>
+              <hr className="dropdown-divider" />
+              <Link to="/projects" className="navbar-item is-size-5">
+                <FontAwesomeIcon className="mr-3" icon={faProjectDiagram} />
+                Projects
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <a className="navbar-item is-size-4">
+            User <FontAwesomeIcon className="ml-3" icon={faUser} />
+          </a>
+        </div>
+      </nav>
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;
